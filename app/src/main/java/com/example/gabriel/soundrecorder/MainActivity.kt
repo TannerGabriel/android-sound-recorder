4b76b9aeb1a05e97e91aaa63ae13a4b7d137a3a5
Gabriel Tanner <gabrieltanner.code@gmail.com>
2018-12-06 20:44:09 +0100

    Added record feature

--- app/src/main/java/com/example/gabriel/soundrecorder/MainActivity.kt
+++ app/src/main/java/com/example/gabriel/soundrecorder/MainActivity.kt
@@ -1,12 +1,82 @@
 package com.example.gabriel.soundrecorder
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private var output: String? = null
+    private var mediaRecorder: MediaRecorder? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaRecorder = MediaRecorder()
+        output = Environment.getExternalStorageDirectory().absolutePath + "/recording.mp3"
+
+
+
+
+
+        fab_start_recording.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                ActivityCompat.requestPermissions(this, permissions,0)
+
+
+            } else {
+                mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+                mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+                mediaRecorder?.setOutputFile(output)
+
+                startRecording()
+            }
+        }
+
+        fab_stop_recording.setOnClickListener{
+            stopRecording()
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun startRecording() {
+        try {
+            mediaRecorder?.prepare()
+            mediaRecorder?.start()
+
+            fab_start_recording.visibility = View.INVISIBLE
+            fab_stop_recording.visibility = View.VISIBLE
+            Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun stopRecording(){
+        mediaRecorder?.stop()
+        mediaRecorder?.release()
+
+        fab_start_recording.visibility = View.VISIBLE
+        fab_stop_recording.visibility = View.INVISIBLE
     }
 }

b3162034c9419ea5877d23b1d3df046f2e6124b6
Gabriel Tanner <gabrieltanner.code@gmail.com>
2018-12-11 14:17:39 +0100

    UI and sound fixes

--- app/src/main/java/com/example/gabriel/soundrecorder/player/RecordingRepository.kt
+++ app/src/main/java/com/example/gabriel/soundrecorder/player/RecordingRepository.kt
@@ -5,6 +5,8 @@ import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Environment
+import android.support.v4.content.ContextCompat.getSystemService
+import android.widget.Toast
 import java.io.File
 import java.nio.file.Files
 
@@ -21,12 +23,20 @@ class RecordingRepository{
 
         fun playRecording(context: Context, title: String){
             val path = Uri.parse(Environment.getExternalStorageDirectory().absolutePath+"/soundrecorder/$title")
-            val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
-                setAudioStreamType(AudioManager.STREAM_MUSIC)
-                setDataSource(context, path)
-                prepare()
-                start()
+
+
+            val manager: AudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+            if(manager.isMusicActive) {
+                Toast.makeText(context, "Another recording is just playing! Wait until it's finished!", Toast.LENGTH_SHORT).show()
+            }else{
+                val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
+                    setAudioStreamType(AudioManager.STREAM_MUSIC)
+                    setDataSource(context, path)
+                    prepare()
+                    start()
+                }
             }
+
         }
     }
 
